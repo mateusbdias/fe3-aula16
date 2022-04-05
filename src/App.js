38b92cd88d0e1a97f72ed7acccfe985eaa83e109
Mateus Benites Dias <imattdias@gmail.com>
2022-04-05 15:19:48 -0300

    CTD - Front End III - Aula 17 - Exercício Sync

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+import Initial from "./pages/Initial";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/details">Details</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Initial />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <footer>
+        <span>Todos os direitos reservados</span>
+      </footer>
+    </BrowserRouter>
   );
 }
-
-export default App;
